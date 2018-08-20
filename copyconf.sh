7d5b98fdb553679d23792260a05867b11d8b96dd
Robert L Quigley <lexm@pobox.com>
2018-08-20 07:22:15 -0400

    Add sakura bash and i3

--- copyconf.sh
+++ copyconf.sh
@@ -7,3 +7,16 @@ fi
 if [[ -f ~/.bashrc ]]; then
 	cp -u ~/.bashrc conffiles/bashrc
 fi
+
+if [[ -d ~/.config ]]; then
+	if [[ -d ~/.config/sakura ]]; then
+		# make sure the sakura folder exists
+		mkdir -p conffiles/config/sakura
+		cp ~/.config/sakura/sakura.conf conffiles/config/sakura/sakura.conf
+	fi
+
+	if [[ -d ~/.config/i3 ]]; then
+		mkdir -p conffiles/config/i3
+		cp ~/.config/i3/config conffiles/config/i3/config
+	fi
+fi
